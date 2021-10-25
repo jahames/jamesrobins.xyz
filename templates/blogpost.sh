49288e9ceb87cee6d4f262295effc5e41b165da7
Jahames <knockknockitsjimmy@outlook.com>
2021-10-25 19:19:45 +1000

    2nd blog post

--- templates/blogpost.sh
+++ templates/blogpost.sh
@@ -1,4 +1,4 @@
-title="My First Blog Post!"
-content="I've never been one to write things down, but I might do so from here onwards. Check in to get some of my thoughts on random stuff."
+title="Blog Post Bash Script!"
+content="I wrote a Bash script that uploads blog posts for me. I used awk which, to be honest, I don't know that well. It's really surprising how much you can do with so few lines of code... and other people's programs as well (lol).</p>\n        <p>I think I might make a similar script for my projects page. That's going to need much more complicated templates though. Oh well. We'll get there.</p>\n        <p>tldr: All you need is bash.</p>\n        <p>Thanks for reading."
 signature="-James Robins Oct 25 2021"
 date="Oct 25 2021"
